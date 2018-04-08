56d2fce12b78d693c61c6ae7f84dc39f79b1e8b4
daniel <daniel@bogusemailserver.com>
2018-04-08 18:49:35 +0200

    docs

--- lesson2-signatures/impl/Str/String.hs
+++ lesson2-signatures/impl/Str/String.hs
@@ -2,7 +2,13 @@ module Str.String where
 
 import qualified Data.List.Split
 
+-- type synonym to make the names match
 type Str = String
 
+-- the names and the types must match with the signature
 splitOn :: Char -> Str -> [Str]
 splitOn c = Data.List.Split.splitOn [c]
+
+-- implementation modules can have other functions and types defined
+blah :: String
+blah = "blah"
--- lesson2-signatures/impl/Str/Text.hs
+++ lesson2-signatures/impl/Str/Text.hs
@@ -2,7 +2,9 @@ module Str.Text where
 
 import qualified Data.Text
 
+-- type synonym to make the names match
 type Str = Data.Text.Text
 
+-- the names and the types must match with the signature
 splitOn :: Char -> Str -> [Str]
 splitOn c = Data.Text.splitOn (Data.Text.singleton c)
